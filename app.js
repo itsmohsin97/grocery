c551fad21a467f89ccee04927b583a5f5acd806c
mohsinsultan <mohsinsultan14k@gmail.com>
2022-12-11 21:37:08 +0530

    electron_auto_update

--- app.js
+++ app.js
@@ -1,16 +1,37 @@
 const { app, BrowserWindow } = require('electron')
 const url = require("url");
 const path = require("path");
-
+const { autoUpdater } = require('electron-updater')
+const log = require('electron-log');
+log.transports.file.resolvePath = () => path.join('I:/Angular/finalgrocery/logs', 'logs/main.log');
+log.info("hello ,Log");
+log.warn("some problem appears");
 let mainWindow
 
 function createWindow() {
   mainWindow = new BrowserWindow({
-    width: 800,
-    height: 600,
+    width: 1200,
+    height: 1000,
     webPreferences: {
       nodeIntegration: true
     }
+  })
+  app.on('ready', () => {
+    createWindow()
+    autoUpdater.checkForUpdatesAndNotify()
+  })
+
+  autoUpdater.on("update-available", () => {
+    log.info("update-avaliable")
+  })
+  autoUpdater.on("checking-for-update", () => {
+    log.info("checking-for-update")
+  })
+  autoUpdater.on("update-not-available", () => {
+    log.info("update-not-available")
+  })
+  autoUpdater.on("update-downloaded", () => {
+    log.info("update-downloaded")
   })
 
   mainWindow.loadURL(
